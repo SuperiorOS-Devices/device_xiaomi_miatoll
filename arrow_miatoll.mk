649eb3793d11090a938812e962692483828640dd
Sonal Singh <sonal.singh.19993@gmail.com>
2021-10-17 03:36:59 +0200

    miatoll: Switch to walleye FP and Desc
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

---
+++ arrow_miatoll.mk
@@ -0,0 +1,31 @@
+#
+# Copyright (C) 2020 The ArrowOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from miatoll  device
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+
+# Inherit some common ArrowOS stuff
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier
+PRODUCT_NAME := arrow_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Miatoll
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Maintainer
+DEVICE_MAINTAINER := SonalSingh18
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
+
+BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys

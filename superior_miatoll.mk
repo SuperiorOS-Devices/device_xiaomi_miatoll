6f488eeed266db964fd121057832c50ad30d86f0
HaSSaN <hasun.mohl@gmail.com>
2021-11-29 13:15:08 +0000

    miatoll: Superior BringUp

--- superior_miatoll.mk
+++ superior_miatoll.mk
@@ -7,14 +7,20 @@
 # Inherit from miatoll  device
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common SuperiorOS stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Charging Animation
+TARGET_INCLUDE_PIXEL_CHARGER := true
+
+# Face unlock
+TARGET_USES_FACE_UNLOCK := true
+
 # Device identifier
-PRODUCT_NAME := arrow_miatoll
+PRODUCT_NAME := superior_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Miatoll
@@ -22,8 +28,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Maintainer
-DEVICE_MAINTAINER := SonalSingh18
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys"
